e68707b37d2f30a7433dbeec35fb6d375a59039b
hhelly <honeygrammer@naver.com>
2021-11-30 22:00:38 +0900

    02_if문 No.2753 윤년 풀이

--- src/main/java/if_02/LeapYear.java
+++ src/main/java/if_02/LeapYear.java
@@ -21,8 +21,19 @@ package if_02;
  *
  * */
 
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
 
+        if(year % 400 == 0) {
+            System.out.println(1);
+        } else if(year % 4 == 0 && year % 100 != 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 }
