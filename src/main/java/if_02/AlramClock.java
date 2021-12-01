836459ffa9f52361d0439d25fd1e53b5c08a4937
hhelly <honeygrammer@naver.com>
2021-12-01 22:49:28 +0900

    02_if문 No.2864 알람시계 풀이

--- src/main/java/if_02/AlramClock.java
+++ src/main/java/if_02/AlramClock.java
@@ -26,8 +26,26 @@ package if_02;
  *  22 55
  * */
 
+import java.util.Scanner;
+
 public class AlramClock {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if( m < 45 ) {
+            h -= 1;
+            m = m + 15;
+
+            if( h < 0 ) {
+                h += 24;
+            }
+        } else {
+            m -= 45;
+        }
+
+        System.out.println(h + " " + m);
     }
 }
