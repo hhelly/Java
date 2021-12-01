c5f52afde40a937dd77d6bb6030f2e372a28bdc9
hhelly <honeygrammer@naver.com>
2021-12-01 23:05:03 +0900

    03_for문 No.2739 구구단 풀이

--- src/main/java/for_03/MultiplicationTable.java
+++ src/main/java/for_03/MultiplicationTable.java
@@ -23,8 +23,16 @@ package for_03;
  * */
 
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        for(int i = 1; i < 10 ; i++) {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
     }
 }
